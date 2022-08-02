612f042426535af958e8bb0b5eb98f33d270b380
Ana Lúcia Medeiros <ana.medeiros@exploitcapital.com.br>
2022-08-02 08:46:27 -0300

    displaying products

--- desafio/src/pages/Home/index.js
+++ desafio/src/pages/Home/index.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    Row,
-    Col
-  } from 'react-bootstrap';
-  import { Container, BoxProducts } from './styles';
+  Row,
+  Col
+} from 'react-bootstrap';
+import { Container, BoxProducts } from './styles';
 
-  //components
-  import Header from '../../components/Header'
-  import Product from '../../components/Product'
+//components
+import Header from '../../components/Header'
+import Product from '../../components/Product'
 
-  export default function Home() {
-      return(
-        <>
-          <Container>
-            <Row>
-              <Col xs={12}>
-                <Header />
-              </Col>
-              <h2>Produtos</h2>
-              <BoxProducts>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-                <Product props/>
-              </BoxProducts>  
-            </Row>
-          </Container>
-        </>
-      );
-  }
+export default function Home() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch('https://62d742f351e6e8f06f1a83da.mockapi.io/api/produtos').then(res => res.json()).then(data => setProducts(data));
+  }, [])
+
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col xs={12}>
+            <Header />
+          </Col>
+          <h2>Produtos</h2>
+          <BoxProducts>
+            {products.map((product) => (
+              <Product title={product.name} description={product.desciption} price={product.price} img={product.avatar} />
+            ))}
+          </BoxProducts>
+        </Row>
+      </Container>
+    </>
+  );
+}
